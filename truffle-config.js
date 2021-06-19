16ba252b3dfb6588d521664a1839d34a77a0a05b
kamilsadik <75034988+kamilsadik@users.noreply.github.com>
2021-06-19 08:24:50 -0400

    added development to config

--- truffle-config.js
+++ truffle-config.js
@@ -50,13 +50,14 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/b21953f6533c42fcab0899f24c94e690")
       },
       network_id: 4 // `network_id` for the Rinkeby network.
-    }
+    },
 
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    // Configuration for Ganache development network
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
