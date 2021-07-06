983f9b1f84f93ae9fa2e1ebd4bcfa40c9e16a7e0
kamilsadik <75034988+kamilsadik@users.noreply.github.com>
2021-07-06 09:02:33 -0400

    Refactored CreatorTokenExchange computation functions into CreatorTokenComputation so as not to exceed the maximum contract size.

--- contracts/creatortokencomputation.sol
+++ contracts/creatortokencomputation.sol
@@ -1,41 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./creatortokencomputation.sol";
+import "./creatortokenownership.sol";
 
-contract CreatorTokenExchange is CreatorTokenComputation {
+contract CreatorTokenComputation is CreatorTokenOwnership {
 
-	constructor(string memory uri) CreatorTokenComputation(uri) { }
-
-	// Event that fires when a new transaction occurs
-	event NewTransaction(address indexed account, uint amount, string transactionType, uint tokenId, string name, string symbol);
-
-	// Allow user to buy a given CreatorToken from the platform
-	function buyCreatorToken(uint _tokenId, uint _amount) external payable {
-		// Calculate proceedsRequired (not including fee)
-		//uint proceedsRequired = _buyProceeds(_tokenId, _amount);
-		// Calculate fee
-		//uint feeRequired = _feeProceeds(proceedsRequired);
-		// Make sure that user sends proceedsRequired wei to cover the cost of _amount tokens, plus the platform fee
-		//require(msg.value == _totalProceeds(proceedsRequired, feeRequired));//== 2000000000000000000);//
-
-		// Compute total transactin proceeds required (inclusive of fee)
-		uint totalProceeds = _totalProceeds(_tokenId, _amount)[2];
-		// Require that user sends totalProceeds in order to transact
-		require(msg.value == totalProceeds);
-
-		// Mint _amount tokens at the user's address (note this increases token amount outstanding)
-		mint(msg.sender, _tokenId, _amount, "");
-		// Emit new transaction event
-		emit NewTransaction(msg.sender, _amount, "buy", _tokenId, creatorTokens[_tokenId].name, creatorTokens[_tokenId].symbol);
-		// Check if new outstanding amount of token is greater than maxSupply
-		if (creatorTokens[_tokenId].outstanding > creatorTokens[_tokenId].maxSupply) {
-			// Update maxSupply
-			creatorTokens[_tokenId].maxSupply = creatorTokens[_tokenId].outstanding;
-			// Call _payout to transfer excess liquidity
-			_payCreator(_tokenId, creatorTokens[_tokenId].creatorAddress);
-		}
-	}
+	constructor(string memory uri) CreatorTokenOwnership(uri) { }
 
 	// Calculate total transaction proceeds
 	function _totalProceeds(uint _tokenId, uint _amount) public returns (uint256[3] memory) {
@@ -95,32 +65,8 @@ contract CreatorTokenExchange is CreatorTokenComputation {
 		return area;
 	}
 
-	// Allow user to sell a given CreatorToken back to the platform
-	function sellCreatorToken(uint _tokenId, uint _amount, address payable _seller) external payable {
-		// Require that user calling function is selling own tokens
-		require(_seller == msg.sender);
-		// Initialize proceeds required
-		uint proceedsRequired = 0;
-		// Initialize pre-transaction supply
-		uint startingSupply = creatorTokens[_tokenId].outstanding;
-		// Compute sale proceeds required
-		proceedsRequired = _saleFunction(startingSupply, _amount, mNumerator, mDenominator, creatorTokens[_tokenId].maxSupply, profitMargin);
-		// Compute fee
-		uint fee = proceedsRequired*platformFee/100;
-		// Add platform fee to obtain real proceedsRequired value
-		proceedsRequired -= fee;
-		// Burn _amount tokens from user's address (note this decreases token amount outstanding)
-		burn(_seller, _tokenId, _amount);
-		// Send user proceedsRequired wei (less the platform fee) in exchange for the burned tokens
-		_seller.transfer(proceedsRequired); //1500000000000000000); //
-		// Update platform fee total
-		_platformFeeUpdater(fee);
-		// Emit new transaction event
-		emit NewTransaction(msg.sender, _amount, "sell", _tokenId, creatorTokens[_tokenId].name, creatorTokens[_tokenId].symbol);
-	}
-
 	// Calculate area under sale price function
-	function _saleFunction(uint _startingSupply, uint _amount, uint _mNumerator, uint _mDenominator, uint _maxSupply, uint _profitMargin) private pure returns (uint256) {
+	function _saleFunction(uint _startingSupply, uint _amount, uint _mNumerator, uint _mDenominator, uint _maxSupply, uint _profitMargin) internal pure returns (uint256) {
 		// Calculate breakpoint and endSupply
 		(uint a, uint b, uint endSupply) = _breakpoint(_startingSupply, _amount, _mNumerator, _mDenominator, _maxSupply, _profitMargin);
 		// Initialize area under curve
@@ -142,7 +88,7 @@ contract CreatorTokenExchange is CreatorTokenComputation {
 	}
 
 	// Calculate breakpoint and other key inputs into _saleFunction
-	function _breakpoint(uint _startingSupply, uint _amount, uint _mNumerator, uint _mDenominator, uint _maxSupply, uint _profitMargin) private pure returns (uint, uint, uint) {
+	function _breakpoint(uint _startingSupply, uint _amount, uint _mNumerator, uint _mDenominator, uint _maxSupply, uint _profitMargin) internal pure returns (uint, uint, uint) {
 		// Define breakpoint (a,b) chosen s.t. area under sale price function is (1-profitMargin) times area under buy price function
 		uint a = _maxSupply/2;
 		uint b = _maxSupply*(50-_profitMargin)/100*_mNumerator/_mDenominator;
@@ -151,7 +97,7 @@ contract CreatorTokenExchange is CreatorTokenComputation {
 	}
 
 	// _saleFunction scenario in which entire transaction occurs left of breakpoint
-	function _leftArea(uint _a, uint _b, uint _startingSupply, uint _endSupply) private pure returns (uint256) {
+	function _leftArea(uint _a, uint _b, uint _startingSupply, uint _endSupply) internal pure returns (uint256) {
 			//uint base1 = ((_b/_a)*(_a-_endSupply)+_b);
 			//uint base1 = ((_b/_a)*(_endSupply-_a)+_b);
 			uint base1 = (_b-(_b/_a)*(_a-_endSupply));
@@ -163,7 +109,7 @@ contract CreatorTokenExchange is CreatorTokenComputation {
 	}
 
 	// _saleFunction scenario in which transaction crosses breakpoint
-	function _bothArea(uint _a, uint _b, uint _startingSupply, uint _endSupply, uint _mNumerator, uint _mDenominator, uint _maxSupply) private pure returns (uint256) {
+	function _bothArea(uint _a, uint _b, uint _startingSupply, uint _endSupply, uint _mNumerator, uint _mDenominator, uint _maxSupply) internal pure returns (uint256) {
 		//uint leftBase1 = ((_b/_a)*(_a-_endSupply)+_b);
 		//uint leftBase1 = ((_b/_a)*(_endSupply-_a)+_b);
 		uint leftBase1 = (_b-(_b/_a)*(_a-_endSupply));
@@ -177,32 +123,10 @@ contract CreatorTokenExchange is CreatorTokenComputation {
 	}
 
 	// _saleFunction scneario in which entire transaction occurs right of breakpoint
-	function _rightArea(uint _a, uint _b, uint _startingSupply, uint _endSupply, uint _mNumerator, uint _mDenominator, uint _maxSupply) private pure returns (uint256) {
+	function _rightArea(uint _a, uint _b, uint _startingSupply, uint _endSupply, uint _mNumerator, uint _mDenominator, uint _maxSupply) internal pure returns (uint256) {
 		uint base1 = (((_mNumerator*_maxSupply/_mDenominator-_b)/(_maxSupply-_a))*(_endSupply-_a)+_b);
 		uint base2 = (((_mNumerator*_maxSupply/_mDenominator-_b)/(_maxSupply-_a))*(_startingSupply-_a)+_b);
 		uint height = _startingSupply-_endSupply;
 		return (base1 + base2) * height / 2;
-	}
-
-	// Transfer excess liquidity (triggered only when a CreatorToken hits a new maxSupply)
-	function _payCreator(uint _tokenId, address payable _creatorAddress) internal {
-		// Create a variable showing excess liquidity that has already been transferred out of this token's liquidity pool
-		uint alreadyTransferred = tokenValueTransferred[_tokenId];
-		// Initialize totalProfit
-		uint totalProfit = 0;
-		// Calculate totalProfit (area between b(x) and s(x) from 0 to maxSupply)
-		totalProfit = _buyFunction(0, creatorTokens[_tokenId].maxSupply, mNumerator, mDenominator) - _saleFunction(creatorTokens[_tokenId].maxSupply, creatorTokens[_tokenId].maxSupply, mNumerator, mDenominator, creatorTokens[_tokenId].maxSupply, profitMargin);
-		// Calculate creator's new profit created from new excess liquidity created
-		uint newProfit = totalProfit - alreadyTransferred; //400000000000000000; // 
-		// Transfer newProfit wei to creator
-		_creatorAddress.transfer(newProfit);
-		// Update amount of value transferred to creator
-		tokenValueTransferred[_tokenId] = totalProfit;
-	}
-
-	// Update platform fees tracker
-	function _platformFeeUpdater(uint _fee) internal {
-		totalPlatformFees += _fee;
-		platformFeesOwed += _fee;
 	}
 }
