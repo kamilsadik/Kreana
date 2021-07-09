2a5234e3a48b4c64564a699c57acbfe5ac320cc1
kamilsadik <75034988+kamilsadik@users.noreply.github.com>
2021-07-09 10:33:21 -0400

    Cleaning up comments

--- contracts/creatortokenfactory.sol
+++ contracts/creatortokenfactory.sol
@@ -21,9 +21,6 @@ contract CreatorTokenFactory is Ownable {
 	// Slope of buy price function
 	uint mNumerator = 9000000000000;
 	uint mDenominator = 185;
-	//uint mNumerator = 9;
-	//uint mDenominator = 10000;
-	//uint m = mNumerator/mDenominator;
 
 	struct CreatorToken {
 		address payable creatorAddress;
