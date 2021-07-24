bc94700b24a6921412d5d92d2f66782fa0ae89b7
kamilsadik <75034988+kamilsadik@users.noreply.github.com>
2021-07-24 10:32:51 -0400

    Going to try to consolidate all state-dependent functionality

--- client/src/Content.jsx
+++ client/src/Content.jsx
@@ -4,6 +4,9 @@ import { Grid } from "@material-ui/core";
 import tokenList from "./constants";
 
 const Content = () => {
+
+  
+
   const getTokenCard = tokenObj => {
     return (
       <Grid item xs={12} sm={4}>
