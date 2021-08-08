217eea42dcb9a989e4b0b2feac4f58df69c4a112
kamilsadik <75034988+kamilsadik@users.noreply.github.com>
2021-08-08 17:10:00 -0400

    trying to work on holdings display

--- client/src/DisplayHoldings.jsx
+++ client/src/DisplayHoldings.jsx
@@ -16,6 +16,16 @@ const DisplayHoldings = () => {
     setTokenState(await tokens)
   }, []);
 
+  function mapHoldings(holdings, tokens) {
+    let holdingsMap = [];
+    for (let i=0; i<tokens.length; i++) {
+      for (let j=0; j<holdings.length; j++) {
+        holdingsMap.push({i:j});
+      }
+    }
+    return(holdingsMap);
+  }
+
   return (
     <Grid container spacing={1}>
       {holdingsState}
