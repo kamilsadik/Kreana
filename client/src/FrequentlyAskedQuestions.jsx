722b8859216a655eaf66527310824c7edac419e5
kamilsadik <75034988+kamilsadik@users.noreply.github.com>
2021-08-16 15:40:56 -0400

    updated faq

--- client/src/FrequentlyAskedQuestions.jsx
+++ client/src/FrequentlyAskedQuestions.jsx
@@ -8,22 +8,24 @@ const data = {
     rows: [
         {
             title: "What is Kreana?",
-            content: `Krena is a Ethereum-based fundraising platform and marketplace for creators and their fans. Creators mint their fundraising
-            token, and fans can buy the token of their favorite creators. Creators can offer perks like 1:1 Zooms, backstage passes,
-            and more to holders of their tokens. Fans benefit from guaranteed liquidity on their creator token holdings,`,
+            content: `Krena is a Ethereum-based fundraising platform and marketplace for creators and their fans. Creators can mint a fundraising
+            token, and fans can buy and sell the tokens of their favorite creators. To incentivize token ownership, creators can offer perks like 1:1 Zooms,
+            backstage passes, and more to holders of their tokens. Fans benefit from guaranteed liquidity on their creator token holdings, and the
+            potential for price appreciation as their favorite creator's following grows.`,
         },
         {
             title: "How does it work?",
-            content: `Kreana uses a novel mechanism called a Dynamic Automated Market Maker (DAMM). The DAMM is a variation of the
+            content: `Kreana uses a novel mechanism called a  <a href="https://github.com/kamilsadik/Kreana/blob/main/README.md" target="_blank">Dynamic Automated Market Maker (DAMM)</a>. The DAMM is a variation of a
             traditional bonding curve which allows for a fixed percentage of the liquidity pool to be routed toward a fundraising goal,
             while the remainder of the pool serves to provide smooth and continuous liquidity on the underlying token. Creator tokens
-            conform to the ERC-1155 token standard.`,
+            conform to the <a href="https://docs.openzeppelin.com/contracts/3.x/erc1155" target="_blank">ERC-1155 Multi Token Standard</a>.`,
         },
         {
             title: "What do I need in order to get started?",
             content: `In order to create a token, or to buy and sell a creator's token, you'll need an Ethereum wallet that allows you
             to interact with Ethereum blockchain apps. We recommend <a href="https://metamask.io/index.html" target="_blank">MetaMask</a>, an easy-to-use wallet
-            built into your browser. Once you've set up your wallet, you'll just need to buy some ETH in order to transact.`,
+            built into your browser. Once you've set up your wallet, you'll just need to buy some ETH in order to transact.\n(Note: For the
+            alpha release on Rinkeby testnet, you can get ETH for free from the <a href="https://faucet.rinkeby.io/" target="_blank">Rinkeby faucet</a>).`,
         },
         {
             title: "How do I create my own token?",
@@ -33,7 +35,7 @@ const data = {
         {
             title: "How do I buy or sell a token?",
             content: `Once your <a href="https://metamask.io/index.html" target="_blank">MetaMask</a> wallet is set up, just
-            click buy or sell on the token you'd like to trade! Note that you can only sell tokens you own.`,
+            click "Buy" or "Sell" on the token you'd like to trade! Note that you can only sell tokens you own.`,
         },
         {
             title: "Where can I get help or send suggestions?",
